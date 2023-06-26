319c50f68f307afb3769e2cf528632ecc4666315
John Basile <ashton.basile@gmail.com>
2023-06-26 15:17:12 +0100

    lecture 6, 14:04

--- lecture6/singlepage1/singlepage1/settings.py
+++ lecture6/singlepage1/singlepage1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sectionExample1',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
