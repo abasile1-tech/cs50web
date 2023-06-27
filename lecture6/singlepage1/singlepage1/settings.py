7553e90776ad3dedf33083658f4c144811e6099e
John Basile <ashton.basile@gmail.com>
2023-06-27 11:48:28 +0100

    lecture6 32:06 infinite scroll

--- lecture6/singlepage1/singlepage1/settings.py
+++ lecture6/singlepage1/singlepage1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts',
     'sectionExample1',
     'django.contrib.admin',
     'django.contrib.auth',
--- lecture6/singlepage1/singlepage1/urls.py
+++ lecture6/singlepage1/singlepage1/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("sectionExample1.urls")),
+    path("posts/", include("posts.urls")),
 ]
